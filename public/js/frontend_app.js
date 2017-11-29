dcb30a28688f3b50dadbb6c5ce7620bbf6f3745e
petercz1 <email.peter.carroll@gmail.com>
2017-11-29 11:32:19 +0800

    Let's just be friends and make no special effort to ever see each other again.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -2,3 +2,9 @@ console.log('loaded frontend app');
 
 var frontend_app = angular.module('patient_app',['ngRoute']);
 
+frontend_app.config(do_routes);
+
+function do_routes($routeProvider) {
+    console.log('doing frontend routes stuff');
+    
+}
