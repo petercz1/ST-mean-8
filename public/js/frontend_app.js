6d863b6a74c017760ebbdc27f43afe2142d7d957
petercz1 <email.peter.carroll@gmail.com>
2017-11-29 11:36:43 +0800

    Laundry is the fifth dimension!!  ... um ... um ... th' washing machine is a black hole and the pink socks are bus drivers who just fell in!!

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,18 +1,18 @@
 console.log('loaded frontend app');
 
 var frontend_app = angular.module('patient_app', ['ngRoute']);
-
 frontend_app.config(do_routes);
+
 
 function do_routes($routeProvider) {
   console.log('doing frontend routes stuff');
   $routeProvider
     .when('/', {
       templateUrl: 'partials/patients.html',
-      controller: do_all_patients
+      controller: all_patients
     })
     .when('/patient/:_id', {
       templateUrl: 'partials/patient_record.html',
-      controller: do_single_patient
+      controller: single_patient
     });
 }
