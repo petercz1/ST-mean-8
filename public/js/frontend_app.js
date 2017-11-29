62caf7d5c36fd8fc59ab2761e2e848a91326ff0d
petercz1 <email.peter.carroll@gmail.com>
2017-11-29 11:35:37 +0800

    Troubled day for virgins over 16 who are beautiful and wealthy and live in eucalyptus trees.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -7,10 +7,12 @@ frontend_app.config(do_routes);
 function do_routes($routeProvider) {
   console.log('doing frontend routes stuff');
   $routeProvider
-    .when('/',{
-
+    .when('/', {
+      templateUrl: 'partials/patients.html',
+      controller: do_all_patients
     })
     .when('/patient/:_id', {
-        
+      templateUrl: 'partials/patient_record.html',
+      controller: do_single_patient
     });
 }
