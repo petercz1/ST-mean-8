c87454efa7d543b1359c674656a600ace65361fb
petercz1 <email.peter.carroll@gmail.com>
2017-11-29 11:45:11 +0800

    Q:	How many Martians does it take to screw in a light bulb? A:	One and a half.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -2,8 +2,8 @@ console.log('loaded frontend app');
 
 var frontend_app = angular.module('patient_app', ['ngRoute']);
 frontend_app.config(do_routes);
-frontend_app.controller('all_patients', do_all_patients);
-frontend_app.controller('single_patient', do_single_patient);
+//frontend_app.controller('all_patients', do_all_patients);
+//frontend_app.controller('single_patient', do_single_patient);
 
 function do_routes($routeProvider) {
   console.log('doing frontend routes stuff');
