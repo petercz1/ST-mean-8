27da2e00e9a20f2b0ba22ffdb9326f4a3be0ae94
petercz1 <email.peter.carroll@gmail.com>
2017-11-29 11:44:05 +0800

    Linux!  Guerrilla UNIX Development     Venimus, Vidimus, Dolavimus. (By mah@ka4ybr.com, Mark A. Horton KA4YBR)

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -22,7 +22,7 @@ function do_all_patients($scope, $http) {
   console.log('getting all patients');
   $http.get('/api/v8/read').then(function (server_object) {
       console.log(server_object);
-      $scope.patent
+      $scope.patients = server_object.data;
   });
 }
 
