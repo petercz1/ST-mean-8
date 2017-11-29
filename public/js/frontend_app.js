2c14962d5a8938ac0b764e199bacfb1673961b81
petercz1 <email.peter.carroll@gmail.com>
2017-11-29 11:37:49 +0800

    linux: because a PC is a terrible thing to waste (ksh@cis.ufl.edu put this on Tshirts in '93)

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -2,7 +2,7 @@ console.log('loaded frontend app');
 
 var frontend_app = angular.module('patient_app', ['ngRoute']);
 frontend_app.config(do_routes);
-
+frontend_app.co
 
 function do_routes($routeProvider) {
   console.log('doing frontend routes stuff');
