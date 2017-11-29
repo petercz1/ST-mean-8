6dce770bb42908bb2341b2811af53d3588a8106a
petercz1 <email.peter.carroll@gmail.com>
2017-11-29 10:07:20 +0800

    DISCLAIMER: Use of this advanced computing technology does not imply an endorsement of Western industrial civilization.

--- routes/main_routes.js
+++ routes/main_routes.js
@@ -34,19 +34,18 @@ function do_get_one(req, res) {
 function do_create(req, res) {
   console.log('creating single patient');
   console.log(req.body);
-  var data = req.body.incoming_patient;
-//   var data = {
-//     name: req.body.name,
-//     gender: req.body.gender,
-//     contact: {
-//       email: req.body.email,
-//       cell: req.body.cell
-//     },
-//     medical: {
-//       drug: req.body.drug,
-//       usage: req.body.uage
-//     }
-//   };
+  var data = {
+    name: req.body.name,
+    gender: req.body.gender,
+    contact: {
+      email: req.body.email,
+      cell: req.body.cell
+    },
+    medical: {
+      drug: req.body.drug,
+      usage: req.body.uage
+    }
+  };
   var patient = new PATIENTCLASS(data);
   patient.save().then();
 }
