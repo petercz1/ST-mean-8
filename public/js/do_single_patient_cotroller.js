50ab23711d775f57e8b56d35a0922029df6f6ede
petercz1 <email.peter.carroll@gmail.com>
2017-11-30 10:09:00 +0800

    I finally went to the eye doctor.  I got contacts.  I only need them to read, so I got flip-ups. 		-- Steven Wright

---
+++ public/js/do_single_patient_cotroller.js
@@ -0,0 +1,25 @@
+
+function do_single_patient($scope, $http, $routeParams) {
+    console.log('getting single patient');
+    console.log($routeParams);
+    $scope.read = function () {
+      console.log('getting patient record from backend');
+      $http.get('/api/v8/read/' + $routeParams._id)
+        .then(function (server_object) {
+          console.log('retrieved patent from backend');
+          $scope.patient = server_object.data;
+        });
+    }
+    $scope.read();
+  
+    $scope.update = function (patient) {
+      console.log(patient);
+      $http.put('/api/v8/update', patient).then(
+        function (server_object) {
+          console.log(server_object);
+          $scope.message = server_object.data.message;
+        }
+      )
+    }
+  
+  }
--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -6,28 +6,3 @@ frontend_app.config(do_routes);
 frontend_app.controller('all_patients', do_all_patients);
 frontend_app.controller('single_patient', do_single_patient);
 
-
-function do_single_patient($scope, $http, $routeParams) {
-  console.log('getting single patient');
-  console.log($routeParams);
-  $scope.read = function () {
-    console.log('getting patient record from backend');
-    $http.get('/api/v8/read/' + $routeParams._id)
-      .then(function (server_object) {
-        console.log('retrieved patent from backend');
-        $scope.patient = server_object.data;
-      });
-  }
-  $scope.read();
-
-  $scope.update = function (patient) {
-    console.log(patient);
-    $http.put('/api/v8/update', patient).then(
-      function (server_object) {
-        console.log(server_object);
-        $scope.message = server_object.data.message;
-      }
-    )
-  }
-
-}
