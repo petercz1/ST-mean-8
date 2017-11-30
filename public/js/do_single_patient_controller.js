bff1e2837476c3e92ff020b2599acba37bcd5973
petercz1 <email.peter.carroll@gmail.com>
2017-11-30 10:29:45 +0800

    We can found no scientific discipline, nor a healthy profession on the technical mistakes of the Department of Defense and IBM. 		-- Edsger Dijkstra

--- public/js/do_single_patient_controller.js
+++ public/js/do_single_patient_controller.js
@@ -7,9 +7,10 @@ function do_single_patient($scope, $http, $routeParams) {
       console.log('getting patient record from backend');
 
       $http.get('/api/v8/read/' + $routeParams._id)
-      
+
         .then(function (server_object) {
           console.log('retrieved patent from backend');
+          console.log(server_object);
           $scope.patient = server_object.data;
         });
     }
