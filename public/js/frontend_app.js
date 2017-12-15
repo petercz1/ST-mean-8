7e270234db67fbc0b0de0c87a83204dadd091643
petercz1 <email.peter.carroll@gmail.com>
2017-12-15 11:41:16 +0800

    The rich get rich, and the poor get poorer. The haves get more, the have-nots die.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -5,4 +5,3 @@ var frontend_app = angular.module('patient_app', ['ngRoute']);
 frontend_app.config(do_routes);
 frontend_app.controller('all_patients', do_all_patients);
 frontend_app.controller('single_patient', do_single_patient);
-
